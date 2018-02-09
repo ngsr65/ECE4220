2440246f6a1fcb25d3b7cc02319ba4bcfd8e8286
Nick <ngsr65@mail.missouri.edu>
2018-02-09 00:27:05 -0600

    Did more work

--- Lab2/Lab2.c
+++ Lab2/Lab2.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
+
+
+//Structure definitions
 struct Data{
-	int rows;
-	int columns;
+	int rows, columns;
 	int** matrix;
 	float time;
-	int** count;
+	int* count;
 } typedef DataMatrix;
+
+struct Threadarguments{
+	DataMatrix *data;
+	int choice, r, c;
+} typedef Threadargs;
+
+//Function prototypes
+void search(int choice, DataMatrix data, int times);
+void threadsearch(void *args);
+
 
 
 int main(int argc, char **argv){
@@ -17,7 +30,10 @@ int main(int argc, char **argv){
 	FILE *pFile;
 	DataMatrix data;
 	char filename[25];
-	int i,r,c;
+	int i,r,c, times;
+	struct timespec time;
+	long timediff;
+
 
 	if (argc == 1){
 		printf("Please enter the name of the file you wish to open: ");
@@ -31,8 +47,7 @@ int main(int argc, char **argv){
 		if (i < 25){
 			filename[i] = '\0';
 		} else {
-			printf("Error! Filename too long. Quitting...");
-			sleep(2);
+			printf("Error! Filename too long. Quitting...\n");
 			return 0;
 		}
 	}
@@ -46,29 +61,38 @@ int main(int argc, char **argv){
 	fscanf(pFile, "%d", &(data.rows));
 	fscanf(pFile, "%d", &(data.columns));
 
+//Errorcheck
+printf("\nThere are %d rows and %d columns.\n", data.rows, data.columns);
+
 	//Allocate memory for the 2D array
-	data.matrix = (int**)malloc(sizeof(int) * data.rows);
+	data.matrix = (int**)malloc(sizeof(int*) * data.rows);
 	for (i = 0; i < data.rows; i++){
-		*(data.matrix + i)  = (int*)malloc(sizeof(int) * data.columns);
+		*(data.matrix + i) = (int*)malloc(sizeof(int) * data.columns);
 	}
 
-	//Read in values
+//Errorcheck
+printf("\nAllocation ran %d times\n", i);
+
+	//Allocate memory for the count array
+	data.count = (int*)malloc(sizeof(int) * 11);
+
+	//Read in values and display
+	printf("\n\nThe numbers are - \n");
 	for (r = 0; r < data.rows; r++){
 		for (c = 0; c < data.columns; c++){
 			fscanf(pFile, "%d", *(data.matrix + r) + c);
-		}
-	}	
-
-	//Display the numbers
-	printf("\nThe numbers are - \n");	
-	for (r = 0; r < data.rows; r++){
-		for (c = 0; c < data.columns; c++){
-			printf("%d ", *(*(data.matrix + r) + c) );
+			printf("%d ", *(*(data.matrix + r) + c));
 		}
 		printf("\n");
-	}
-	
-	
+	}	
+
+	printf("\n\nHow many times would you like to run each search?");
+	printf("\nPlease enter a positive integer: ");
+	scanf("%d", &times);
+
+	//First search
+	printf("\nSearching the data with one thread %d times...\n", times);
+	search(0, data, times);
 	
 
 
@@ -79,10 +103,116 @@ int main(int argc, char **argv){
 		free(*(data.matrix + i));
 	}
 	free(data.matrix);
+	free(data.count);
 
 	//Close the file
 	fclose(pFile);
 
 
 	return 0;
+}
+
+void search(int choice, DataMatrix data, int times){
+
+	//Variable initalization
+	int i,j, threads;
+	long starttime, difftime[times], timeavg;
+	struct timespec time;
+	Threadargs args;
+	pthread_t *thread;
+
+	//Run the search multiple times
+	for (i = 0; i < times; i++){
+
+		//Set the count array's values to zero
+		for (j = 0; j < 11; j++){
+			*(data.count + j) = 0;
+		}
+
+		//Run with the correct amount of threads for each test
+		switch (choice){
+
+			//One single thread
+			case 0:
+				//Allocate space for the threads
+				thread = (pthread_t*)malloc(sizeof(pthread_t) * 1);
+				threads = 1;
+				
+				//Set the threads arguments
+				args.data = &data;
+				args.choice = 0;
+
+				//Start the clock
+				clock_gettime(CLOCK_MONOTONIC, &time);
+				starttime = time.tv_nsec;
+
+				//Create the thread
+				pthread_create(&thread, NULL, &threadsearch, (void*)&args);
+
+				//Wait for the thread to finish
+				pthread_join(&thread, NULL);
+
+				break;
+
+			//One thread for each row
+			case 1:
+				
+				break;
+
+			//One thread for each column
+			case 2:
+				
+				break;
+
+			//One thread for each element
+			case 3:
+				
+				break;
+		}
+
+		//Stop the clock and figure out the difference in time
+		clock_gettime(CLOCK_MONOTONIC, &time);
+		difftime[i] = time.tv_nsec - starttime;
+
+		//Free the memory used by the threads
+		free(thread);
+
+		//Output the count and the time it took
+		printf("\n\nTime = %ld nanoseconds", difftime[i]);
+		printf("\nCount  0   1   2   3   4   5   6   7   8   9   10");
+		printf("\n       %-3d ", *(data.count));
+		for (j = 1; j < 11; j++){
+			printf("%-3d ", *(data.count + j));
+		}
+	}
+
+	//Calculate the average time
+	timeavg = 0;
+	for (i = 0; i < times; i++){
+		timeavg += difftime[i];
+	}
+	timeavg /= (long)times;
+
+}
+
+void threadsearch(void *args){
+	
+	//int i, j, rows = *(args->data->rows), columns = *(args->data->columns), choice = *(args->choice);
+	int i, j, rows = ((Threadargs*)args)->data->rows, columns = ((Threadargs*)args)->data->columns, choice = ((Threadargs*)args)->choice;
+
+	printf("\nRows = %d, Columns = %d, Choice = %d", rows, columns, choice);
+
+
+	switch(choice){
+		case 0:
+			for (i = 0; i < rows; i++){
+				for (j = 0; j < columns; j++){
+					//Increment the counter value
+					//*(((Threadargs*)args)->data->count + *(*(((Threadargs*)args)->data->matrix + i) + j))++;
+				}
+			}
+		break;
+	}
+
+	pthread_exit(0);
 }
