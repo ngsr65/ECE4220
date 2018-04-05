286b4d0bd5c6d52a9db149d0a37b2ad0bcb34015
Nick <ngsr65@mail.missouri.edu>
2018-04-05 14:42:51 -0500

    Did some more

--- Lab5/Lab5.c
+++ Lab5/Lab5.c
@@ -8,47 +8,34 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <ifaddrs.h>
+#include <string.h>
 
 //Broadcast IP - 128.206.19.255
 
 int main(int argc, char** argv){
-	int sock, i, votes[10] = {0}, mynum, boolv = 1, ismaster = 0;
-	char msg[40];
+	int sock, i, votes[10] = {0}, mynum, boolv = 1, ismaster = 0, s, family;
+	char msg[40], host[NI_MAXHOST];
 	struct sockaddr_in mysock, *temp;	
-	struct ifaddrs *ipinfo;
-	unsigned long myip;
-	struct in_addr tempinaddr;	
+	struct ifaddrs *ifaddr, *ifa;	
 
 	if (argc != 2){
 		printf("\nError! \"./a.out [port]\"\n");
+		return 1;
 	}
 
+
+
 	//Get my network information
-	/*int main()
-{
-    int fd;
-    struct ifreq ifr;
+	getifaddrs(&ifaddr);
+	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next){
+        	if (ifa->ifa_addr == NULL){
+            		continue;
+		}
+        	s=getnameinfo(ifa->ifa_addr,sizeof(struct sockaddr_in),host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+    	}
 
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    ifr.ifr_addr.sa_family = AF_INET;
-
-    snprintf(ifr.ifr_name, IFNAMSIZ, "eth0");
-
-    ioctl(fd, SIOCGIFADDR, &ifr);
-
-    /* and more importantly */
-    printf("%s\n", inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-
-    close(fd);
-}
-*/
-	getifaddrs(&ipinfo);
-	temp = (struct sockaddr_in *)(ipinfo->ifa_addr);
-	myip = temp->sin_addr.s_addr;
-
-	tempinaddr.s_addr = myip;
-	printf("My IP is %s\n", inet_ntoa(tempinaddr));
+	printf("My IP is %s\n", host);
 
 	//Create the socket
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
@@ -59,7 +46,7 @@ int main(int argc, char** argv){
 	//Set the socket parameters
 	mysock.sin_family = AF_INET;
 	mysock.sin_port = htons(atoi(*(argv + 1)));
-	mysock.sin_addr.s_addr = myip;
+	inet_aton(host, &mysock.sin_addr);
 
 	//Let the socket broadcast
 	if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolv, sizeof(boolv)) < 0){
@@ -71,8 +58,9 @@ int main(int argc, char** argv){
 
 
 	//Free allocated memory
-	freeifaddrs(ipinfo);
-	
+	freeifaddrs(ifaddr);
+	freeifaddrs(ifa);
+
 	return 0;
 }
 
